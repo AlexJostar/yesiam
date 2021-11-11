2203a5606becb93eeeb11cce48aff51a6e4f5495
John Doe <you@example.com>
2021-11-12 00:24:30 +0200

    First

---
+++ laba6/laba6/laba6.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Input number" << endl;
+    cin >> a;
+    ((a > 0) || (a = 0)) ? (cout << "Positive") : (cout << "Zero");
+}
